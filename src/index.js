126885b1f0b48f5cc3fe9348ac104be8fc526a3c
muscaiu <muscaiu@yahoo.com>
2019-05-30 09:25:26 +0300

    added modulat routes and next middleware

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 
+import routes from './routes';
+import models from './models';
+
 const app = express();
 app.use(cors());
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('woot')
-})
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1],
+  };
+  next();
+});
+
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
 
 app.listen(process.env.PORT, () =>
   console.log(`API running on http://localhost:${process.env.PORT}`),
