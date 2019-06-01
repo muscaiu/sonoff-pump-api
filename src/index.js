78b10a4c63ab5217395178c2634b82a07709002d
muscaiu <muscaiu@yahoo.com>
2019-06-01 13:50:35 +0300

    good setup for users/messages routes and models

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import express from 'express';
 import cors from 'cors';
 
 import routes from './routes';
-import models from './models';
+import models, { connectDb } from './models';
 
 const app = express();
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
+app.use(async (req, res, next) => {
   req.context = {
     models,
-    me: models.users[1],
+    me: await models.User.findByLogin('rwieruch'),
   };
   next();
 });
@@ -22,6 +22,48 @@ app.use('/session', routes.session);
 app.use('/users', routes.user);
 app.use('/messages', routes.message);
 
-app.listen(process.env.PORT, () =>
-  console.log(`API running on http://localhost:${process.env.PORT}`),
-);
+const eraseDatabaseOnSync = true;
+
+const createUsersWithMessages = async () => {
+  const user1 = new models.User({
+    username: 'rwieruch',
+  });
+  const user2 = new models.User({
+    username: 'ddavids',
+  });
+  const message1 = new models.Message({
+    text: 'Published the Road to learn React',
+    user: user1.id,
+  });
+  const message2 = new models.Message({
+    text: 'Happy to release ...',
+    user: user2.id,
+  });
+  const message3 = new models.Message({
+    text: 'Published a complete ...',
+    user: user2.id,
+  });
+  await message1.save();
+  await message2.save();
+  await message3.save();
+  await user1.save();
+  await user2.save();
+};
+
+connectDb().then(async () => {
+  if (eraseDatabaseOnSync) {
+    await Promise.all([
+      models.User.deleteMany({}),
+      models.Message.deleteMany({}),
+    ])
+      .then(() => console.log('DB erased succesfully'));
+
+    createUsersWithMessages();
+  }
+
+  app.listen(process.env.PORT, () =>
+    console.log(`API running on http://localhost:${process.env.PORT}`),
+  );
+});
+
+
